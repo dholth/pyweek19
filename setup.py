cfb0f80a2837cca64ac902363a76dd4da571f335
Daniel Holth <dholth@fastmail.fm>
2014-10-05 20:48:23 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os.path, sys, codecs, re
+
+from setuptools import setup
+
+setup(name='inca-game',
+      version="0.0.1",
+      description='Collect enough treasure to fill an entire room.',
+      long_description="""A game for pyweek, Oct. 2014.""",
+      classifiers=[
+        ],
+      author='Daniel Holth',
+      author_email='dholth@fastmail.fm',
+      keywords=['pyweek'],
+      license='MIT',
+      packages=[
+          'inca',
+          ],
+      install_requires=['pysdl2-cffi', 'pytmx'],
+      tests_require=['pytest'],
+      include_package_data=True,
+      zip_safe=False,
+      )
+
