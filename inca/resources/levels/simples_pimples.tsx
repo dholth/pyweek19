df28460bd06b1fe59c58b961348aedd0b59f34d3
Daniel Holth <dholth@fastmail.fm>
2014-10-07 22:00:06 -0400

    use shared tileset with properties

---
+++ inca/resources/levels/simples_pimples.tsx
@@ -0,0 +1,194 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="simples_pimples" tilewidth="16" tileheight="16">
+ <image source="simples_pimples.png" trans="5a5268" width="800" height="1280"/>
+ <tile id="76">
+  <properties>
+   <property name="name" value="Hero Brown"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="group" value="Hero Red"/>
+  </properties>
+ </tile>
+ <tile id="88">
+  <properties>
+   <property name="name" value="Hero Blue"/>
+  </properties>
+ </tile>
+ <tile id="126">
+  <properties>
+   <property name="name" value="Heroine Brown"/>
+  </properties>
+ </tile>
+ <tile id="132">
+  <properties>
+   <property name="name" value="Heroine Red"/>
+  </properties>
+ </tile>
+ <tile id="138">
+  <properties>
+   <property name="name" value="Heroine Blue"/>
+  </properties>
+ </tile>
+ <tile id="176">
+  <properties>
+   <property name="name" value="Goblin"/>
+  </properties>
+ </tile>
+ <tile id="182">
+  <properties>
+   <property name="name" value="Goblin Blue"/>
+  </properties>
+ </tile>
+ <tile id="188">
+  <properties>
+   <property name="name" value="Goblin Grey"/>
+  </properties>
+ </tile>
+ <tile id="226">
+  <properties>
+   <property name="name" value="Devil"/>
+  </properties>
+ </tile>
+ <tile id="232">
+  <properties>
+   <property name="name" value="Devil Grey"/>
+  </properties>
+ </tile>
+ <tile id="238">
+  <properties>
+   <property name="name" value="Devil Blue"/>
+  </properties>
+ </tile>
+ <tile id="276">
+  <properties>
+   <property name="name" value="Zombie"/>
+  </properties>
+ </tile>
+ <tile id="282">
+  <properties>
+   <property name="name" value="Zombie Red"/>
+  </properties>
+ </tile>
+ <tile id="288">
+  <properties>
+   <property name="name" value="Zombie Blue"/>
+  </properties>
+ </tile>
+ <tile id="326">
+  <properties>
+   <property name="name" value="Skeleton"/>
+  </properties>
+ </tile>
+ <tile id="332">
+  <properties>
+   <property name="name" value="Skeleton Red"/>
+  </properties>
+ </tile>
+ <tile id="338">
+  <properties>
+   <property name="name" value="Skeleton Yellow"/>
+  </properties>
+ </tile>
+ <tile id="376">
+  <properties>
+   <property name="name" value="Ghost White"/>
+  </properties>
+ </tile>
+ <tile id="382">
+  <properties>
+   <property name="name" value="Ghost Red"/>
+  </properties>
+ </tile>
+ <tile id="388">
+  <properties>
+   <property name="name" value="Ghost Blue"/>
+  </properties>
+ </tile>
+ <tile id="426">
+  <properties>
+   <property name="name" value="Bat"/>
+  </properties>
+ </tile>
+ <tile id="432">
+  <properties>
+   <property name="name" value="Bat Red"/>
+  </properties>
+ </tile>
+ <tile id="438">
+  <properties>
+   <property name="name" value="Bat White"/>
+  </properties>
+ </tile>
+ <tile id="476">
+  <properties>
+   <property name="name" value="Rat"/>
+  </properties>
+ </tile>
+ <tile id="500">
+  <properties>
+   <property name="name" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="501">
+  <properties>
+   <property name="name" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="502">
+  <properties>
+   <property name="name" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="503">
+  <properties>
+   <property name="name" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="504">
+  <properties>
+   <property name="name" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="505">
+  <properties>
+   <property name="name" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="506">
+  <properties>
+   <property name="name" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="507">
+  <properties>
+   <property name="name" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="508">
+  <properties>
+   <property name="name" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="509">
+  <properties>
+   <property name="name" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="510">
+  <properties>
+   <property name="name" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="511">
+  <properties>
+   <property name="name" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="526">
+  <properties>
+   <property name="name" value="Scorpion"/>
+  </properties>
+ </tile>
+</tileset>
